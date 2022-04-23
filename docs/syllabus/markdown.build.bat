e27812fbfaa0d33d9d7227d0b96f57239c2069de
Ugur Coruh <ugur.coruh.tr@gmail.com>
2022-04-23 03:57:10 +0300

    markdown.build.bat and convert-pptx2md.bat added to all week and related folders

--- docs/syllabus/markdown.build.bat
+++ docs/syllabus/markdown.build.bat
@@ -8,7 +8,7 @@ echo file found "%file_name%"
 Powershell.exe marp %file_name% --html --pdf -o %file_name%_slide.pdf --allow-local-files
 Powershell.exe marp %file_name% --html -o %file_name%_slide.html --allow-local-files
 Powershell.exe marp %file_name% --pptx -o %file_name%_slide.pptx --allow-local-files
-Powershell.exe pandoc %file_name% --pdf-engine=xelatex -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
+rem call Powershell.exe pandoc %file_name% --pdf-engine=xelatex -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
 Powershell.exe pandoc %file_name% --pdf-engine=xelatex -f markdown-implicit_figures -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
 Powershell.exe pandoc -o %file_name%_word.docx -f markdown -t docx %file_name%
 Powershell.exe pandoc -o %file_name%_word.pptx -f markdown -t pptx %file_name% 
@@ -19,7 +19,7 @@ echo file found "%file_name%"
 Powershell.exe marp %file_name% --html --pdf -o %file_name%_slide.pdf --allow-local-files
 Powershell.exe marp %file_name% --html -o %file_name%_slide.html --allow-local-files
 Powershell.exe marp %file_name% --pptx -o %file_name%_slide.pptx --allow-local-files
-Powershell.exe pandoc %file_name% --pdf-engine=xelatex -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
+rem call Powershell.exe pandoc %file_name% --pdf-engine=xelatex -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
 Powershell.exe pandoc %file_name% --pdf-engine=xelatex -f markdown-implicit_figures -V colorlinks -V urlcolor=NavyBlue -V toccolor=Red --toc -N -o %file_name%_doc.pdf
 Powershell.exe pandoc -o %file_name%_word.docx -f markdown -t docx %file_name%
 Powershell.exe pandoc -o %file_name%_word.pptx -f markdown -t pptx %file_name% 
